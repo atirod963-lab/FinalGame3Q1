c23af1847e70fc4aa5cbda019fbed87726cbf074
kong1735 <kk.game1735@gmail.com>
2025-11-11 16:24:26 +0700

    Load scene finish

--- Assets/Script/WorkShop/Manager/TriggerLoadScene.cs
+++ Assets/Script/WorkShop/Manager/TriggerLoadScene.cs
@@ -2,13 +2,13 @@
 
 public class TriggerLoadScene : Item
 {
-    public AudioClip BackgroundMusic;
+    //public AudioClip BackgroundMusic;
     public string LoadSceneName;
 
     public override void OnCollect(Player player)
     {
         base.OnCollect(player);
         LoadSceneManager.instance.LoadNewScene(LoadSceneName);
-        SoundManager.instance.PlayMusic(BackgroundMusic);
+        //SoundManager.instance.PlayMusic(BackgroundMusic);
     }
 }
